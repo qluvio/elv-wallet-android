4152ba446356213aba515a1845615c688fed2492
Stav Raviv <stav.raviv@eluv.io>
2023-08-08 12:37:34 -0700

    bump version code to 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "app.eluvio.wallet"
         minSdk = 23
         targetSdk = 33
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
     }
