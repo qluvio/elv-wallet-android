ca1706f0a1e242c9d77bffcb7b675952b2928164
Stav Raviv <stav.raviv@eluv.io>
2024-07-19 15:45:35 -0700

    change curl prints to show url before headers

--- app/src/debug/kotlin/app/eluvio/wallet/di/DebugInterceptorsModule.kt
+++ app/src/debug/kotlin/app/eluvio/wallet/di/DebugInterceptorsModule.kt
@@ -1,5 +1,12 @@
 package app.eluvio.wallet.di
 
+import com.moczul.ok2curl.CommandComponent.Body
+import com.moczul.ok2curl.CommandComponent.Curl
+import com.moczul.ok2curl.CommandComponent.Flags
+import com.moczul.ok2curl.CommandComponent.Header
+import com.moczul.ok2curl.CommandComponent.Method
+import com.moczul.ok2curl.CommandComponent.Url
+import com.moczul.ok2curl.Configuration
 import com.moczul.ok2curl.CurlInterceptor
 import com.moczul.ok2curl.logger.Logger
 import dagger.Module
@@ -27,10 +34,15 @@ object DebugInterceptorsModule {
     @Provides
     @IntoSet
     fun provideCurlInterceptor(): Interceptor {
-        return CurlInterceptor(object : Logger {
-            override fun log(message: String) {
-                Timber.tag("OkHttp").w(message)
-            }
-        })
+        return CurlInterceptor(
+            logger = object : Logger {
+                override fun log(message: String) {
+                    Timber.tag("OkHttp").w(message)
+                }
+            },
+            configuration = Configuration(
+                components = listOf(Curl, Flags, Method, Url, Body, Header)
+            )
+        )
     }
 }
