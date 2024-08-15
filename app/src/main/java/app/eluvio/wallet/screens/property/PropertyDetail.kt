5a4111dc728687b9417809d14437e33fd0885f7f
Stav Raviv <stav.raviv@eluv.io>
2024-08-15 10:26:26 -0700

    Better error handling for PropDetail network failures

--- app/src/main/java/app/eluvio/wallet/screens/property/PropertyDetail.kt
+++ app/src/main/java/app/eluvio/wallet/screens/property/PropertyDetail.kt
@@ -2,7 +2,9 @@ package app.eluvio.wallet.screens.property
 
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
+import app.eluvio.wallet.app.Events
 import app.eluvio.wallet.navigation.MainGraph
+import app.eluvio.wallet.util.rememberToaster
 import app.eluvio.wallet.util.subscribeToState
 import com.ramcosta.composedestinations.annotation.Destination
 
@@ -13,7 +15,16 @@ import com.ramcosta.composedestinations.annotation.Destination
 @Destination(navArgsDelegate = PropertyDetailNavArgs::class)
 @Composable
 fun PropertyDetail() {
-    hiltViewModel<PropertyDetailViewModel>().subscribeToState { vm, state ->
-        DynamicPageLayout(state)
-    }
+    val toaster = rememberToaster()
+    hiltViewModel<PropertyDetailViewModel>().subscribeToState(
+        onEvent = {
+            when (it) {
+                is Events.NetworkError -> toaster.toast(it.defaultMessage)
+                else -> {}
+            }
+        },
+        onState = { vm, state ->
+            DynamicPageLayout(state)
+        }
+    )
 }
--- app/src/main/java/app/eluvio/wallet/screens/property/PropertyDetailViewModel.kt
+++ app/src/main/java/app/eluvio/wallet/screens/property/PropertyDetailViewModel.kt
@@ -2,15 +2,18 @@ package app.eluvio.wallet.screens.property
 
 import androidx.lifecycle.SavedStateHandle
 import app.eluvio.wallet.app.BaseViewModel
+import app.eluvio.wallet.app.Events
 import app.eluvio.wallet.data.entities.v2.MediaPageEntity
 import app.eluvio.wallet.data.entities.v2.MediaPageSectionEntity
 import app.eluvio.wallet.data.entities.v2.SearchFiltersEntity
 import app.eluvio.wallet.data.stores.MediaPropertyStore
 import app.eluvio.wallet.data.stores.PropertySearchStore
 import app.eluvio.wallet.di.ApiProvider
+import app.eluvio.wallet.navigation.NavigationEvent
 import app.eluvio.wallet.navigation.asPush
 import app.eluvio.wallet.screens.destinations.PropertyDetailDestination
 import app.eluvio.wallet.screens.destinations.PropertySearchDestination
+import app.eluvio.wallet.util.logging.Log
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.kotlin.addTo
@@ -47,15 +50,23 @@ class PropertyDetailViewModel @Inject constructor(
         Flowable.combineLatest(pageLayout, searchFilters) { (page, sections), filters ->
             Triple(page, sections, filters)
         }
-            .subscribe { (mainPage, sections, filters) ->
-                updateState {
-                    copy(
-                        backgroundImagePath = mainPage.backgroundImagePath,
-                        searchNavigationEvent = PropertySearchDestination(propertyId).asPush(),
-                        sections = sections(mainPage, sections, filters)
-                    )
+            .subscribeBy(
+                onNext = { (mainPage, sections, filters) ->
+                    updateState {
+                        copy(
+                            backgroundImagePath = mainPage.backgroundImagePath,
+                            searchNavigationEvent = PropertySearchDestination(propertyId).asPush(),
+                            sections = sections(mainPage, sections, filters)
+                        )
+                    }
+                },
+                onError = {
+                    // Note: might be a problem with deeplinks
+                    Log.e("Error loading property detail. Popping screen", it)
+                    fireEvent(Events.NetworkError)
+                    navigateTo(NavigationEvent.GoBack)
                 }
-            }
+            )
             .addTo(disposables)
     }
 
