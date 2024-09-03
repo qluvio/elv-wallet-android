c442a9c390eecad3a6c438572646536f8ce280da
Stav Raviv <stav.raviv@eluv.io>
2024-09-03 14:47:32 -0700

    remove dim from banners

--- app/src/main/java/app/eluvio/wallet/screens/property/items/BannerItem.kt
+++ app/src/main/java/app/eluvio/wallet/screens/property/items/BannerItem.kt
@@ -3,9 +3,17 @@ package app.eluvio.wallet.screens.property.items
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import app.eluvio.wallet.screens.common.ImageCard
+import androidx.compose.ui.layout.ContentScale
+import androidx.tv.material3.ClickableSurfaceDefaults
+import androidx.tv.material3.MaterialTheme
+import androidx.tv.material3.Surface
 import app.eluvio.wallet.screens.property.DynamicPageLayoutState
+import app.eluvio.wallet.theme.LocalSurfaceScale
+import app.eluvio.wallet.theme.borders
+import app.eluvio.wallet.theme.focusedBorder
+import coil.compose.AsyncImage
 
 @Composable
 fun BannerItem(
@@ -13,13 +21,23 @@ fun BannerItem(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    // TODO: ImageCard might be overkill here, we might just want an AsyncImage with a border
-    ImageCard(
-        item.bannerImageUrl,
-        contentDescription = "Banner",
+    Surface(
         onClick = onClick,
-        shape = RectangleShape,
-        modifier = modifier.fillMaxWidth()
-    )
+        border = MaterialTheme.borders.focusedBorder,
+        scale = LocalSurfaceScale.current,
+        shape = ClickableSurfaceDefaults.shape(RectangleShape),
+        colors = ClickableSurfaceDefaults.colors(
+            containerColor = Color.Transparent,
+            focusedContainerColor = Color.Transparent
+        ),
+        modifier = modifier
+    ) {
+        AsyncImage(
+            model = item.bannerImageUrl,
+            contentScale = ContentScale.Crop,
+            contentDescription = "Banner",
+            modifier = modifier.fillMaxWidth()
+        )
+    }
     // TODO: add title/subtitle
 }
