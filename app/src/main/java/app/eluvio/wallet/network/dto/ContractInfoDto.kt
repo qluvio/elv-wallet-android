1e58ec4ebdcc90c682d153b42cd6ae401510f179
Stav Raviv <stav.raviv@eluv.io>
2024-09-06 00:00:31 -0700

    fix R8 bug on Release builds

--- app/src/main/java/app/eluvio/wallet/network/dto/ContractInfoDto.kt
+++ app/src/main/java/app/eluvio/wallet/network/dto/ContractInfoDto.kt
@@ -1,9 +1,11 @@
 package app.eluvio.wallet.network.dto
 
+import androidx.annotation.Keep
 import app.eluvio.wallet.data.entities.ContractInfoEntity
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
+@Keep
 @JsonClass(generateAdapter = true)
 data class ContractInfoDto(
     override val contract: String,
